613d74fb07be6d8907775eb0858f6d2266960ffb
Bryatn Derosier <bryantjderosier@gmail.com>
2021-11-11 23:24:44 -0500

    setting up tokens

---
+++ packages/tokens/build.js
@@ -0,0 +1,32 @@
+const StyleDictionary = require('style-dictionary');
+
+console.log('Build started...');
+console.log('\n==============================================');
+
+
+// REGISTER THE CUSTOM TRANFORMS
+
+StyleDictionary.registerTransform({
+    name: 'sizes/rem', // notice: the name is an override of an existing predefined method (yes, you can do it)
+    type: 'value',
+    matcher: function (token) {
+        // this is an example of a possible filter (based on the "cti" values) to show how a "matcher" works
+        return token.attributes.category === 'sizes'
+    },
+    transformer: function (token) {
+        return `${token.value}rem`;
+    }
+})
+
+StyleDictionary.registerTransformGroup({
+    name: 'custom/css',
+    // notice: here the "size/px" transform is not the pre-defined one, but the custom one we have declared above
+    transforms: ['attribute/cti', 'name/cti/kebab', 'size/rem', 'color/css', 'time/seconds', 'sizes/rem']
+})
+
+const StyleDictionaryExtended = StyleDictionary.extend(__dirname + '/config.json')
+
+StyleDictionaryExtended.buildAllPlatforms()
+
+console.log('\n==============================================')
+console.log('\nBuild completed!')
--- packages/ui-components/src/lib/Button/Button.svelte
+++ packages/ui-components/src/lib/Button/Button.svelte
@@ -5,3 +5,22 @@
 <button>
     <slot />
 </button>
+
+<style>
+    button {
+        box-sizing: border-box;
+        outline: none;
+        border: none;
+        font-size: 14px;
+        text-transform: uppercase;
+        line-height: 20px;
+        font-weight: 600;
+        font-family: 'Inter', sans-serif;
+        height: 48px;
+        padding: 0 24px;
+        border-radius: 4px;
+        color: white;
+        background-color: #AD00FF;
+        letter-spacing: 1.5%;
+    }
+</style>
